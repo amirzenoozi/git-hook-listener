8f430d1e27882f02062749ff1c454e037535eb55
amirzenoozi <amirzenoozi72@gmail.com>
2023-01-14 17:20:38 +0330

    🚨 Fix `pylint` Issue

--- fastApi/app/scripts/telegram.py
+++ fastApi/app/scripts/telegram.py
@@ -9,30 +9,30 @@ from telebot import types
 telegram = telebot.TeleBot(os.getenv('BOT_TOKEN'))
 
 
-def send_message_to_user(chat_id, request, type):
+def send_message_to_user(chat_id, request, req_type):
     """
     Send Message To User On Telegram
 
     Args:
         chat_id (int): Telegram Chat ID
         request (object): Request Payload
-        type (str): Github Payload Type
+        req_type (str): Github Payload Type
     """
     link_button = types.InlineKeyboardButton(text='Open It!', url=request["commits"][0]["url"])
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(link_button)
     message_body = ''
 
-    if type in ('push', 'Push Hook'):
+    if req_type in ('push', 'Push Hook'):
         message_body = f'''New Commit On Your Ripo `{request["repository"]["name"]}` 🦊.
         \n\nCommit Message:
         \n`{request["commits"][0]["message"]}`'''
-    elif type in ('tag_push', 'Tag Push Hook'):
+    elif req_type in ('tag_push', 'Tag Push Hook'):
         message_body = f'''New Tag Push On Your Ripo `{request["repository"]["name"]}` 🦊.
         \n\nTag Message:
         \n`{request["message"]}`'''
     else:
-        message_body = f'''New {type} On Your Ripo `{request["repository"]["name"]}` 🦊.'''
+        message_body = f'''New {req_type} On Your Ripo `{request["repository"]["name"]}` 🦊.'''
 
 
     telegram.send_message(
